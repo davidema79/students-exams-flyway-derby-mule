ef196a6b3cd5442b1fbebde98dfcd546c0a2f6c2
Davide Martorana <davide.martorana@ricston.com>
2018-10-25 11:06:16 +0200

    Created Database, included dependencies, and properties

---
+++ src/main/resources/db/migration/V1.2__InsertProfessors.sql
@@ -0,0 +1,7 @@
+INSERT INTO professors VALUES(1, 'Albert', 'Einsten');
+INSERT INTO professors VALUES(2, 'Marie', 'Curie');
+INSERT INTO professors VALUES(3, 'Giosu', 'Carducci');
+INSERT INTO professors VALUES(4, 'Albert', 'Camus');
+INSERT INTO professors VALUES(5, 'Alan', 'Turing');
+INSERT INTO professors VALUES(6, 'Guillaume', 'De l`Hopital');
+INSERT INTO professors VALUES(7, 'Edgar Allan', 'Poe');
---
+++ src/main/resources/db/migration/V1.3__InsertSubjects.sql
@@ -0,0 +1,7 @@
+INSERT INTO subjects VALUES(1, 'Math');
+INSERT INTO subjects VALUES(2, 'Italian Literature');
+INSERT INTO subjects VALUES(3, 'English Literature');
+INSERT INTO subjects VALUES(4, 'Chemistry');
+INSERT INTO subjects VALUES(5, 'Physics');
+INSERT INTO subjects VALUES(6, 'Philosophy');
+INSERT INTO subjects VALUES(7, 'Computer Science');
